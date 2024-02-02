a639b651eaa40022339fd4d17b8ea7e8a1d5f036
Chakradhar Dwivedi <chakradhar.dwivedi2000@gmail.com>
2024-02-03 05:17:48 +0530

    compile .genz to print an integer

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
 #GENZ LANG
 
 Genz lang compiler written in Go.
+
+Right now it just prints 1 byte Integers. 
+
+#Create a *.genz file and write the following :
+        return 69; //replace 69 with any 1 byte integer.
+
+#run go build -0 gz main.go
+#run ./gz *.genz
+    
+
--- main.go
+++ main.go
@@ -5,7 +5,11 @@ import (
 	"unicode"
 	"bytes"
 )
-
+//type TokenType struct {
+//	int_lit string := "int_lit",
+//	_return string := "return",
+//	semi string := "semi"
+//}
 type Token struct{
 	TokenType string;
 	value string;
@@ -27,7 +31,7 @@ func tokenize(str string) []Token {
 			i++;
 			for unicode.IsPrint(rune(str[i])) && !unicode.IsSpace(rune(str[i])) {
 				b.WriteByte(str[i]);
- 				i++;
+				i++;
 			}
 			i--;
 			if("return" == b.String()){
@@ -60,21 +64,37 @@ func tokenize(str string) []Token {
 	return tokenArray; 
 }
 
+func token_to_asm(tokens []Token) string{
+	var output string = "global _start\n_start:\n";
+	for i:=0;i<len(tokens);i++{
+		 token := tokens[i];
+		if token.TokenType == "return" {
+			if i+1 < len(tokens) && tokens[i+1].TokenType == "int_lit" {
+				if i+2 < len(tokens) && tokens[i+2].TokenType == "semi" {
+					output += "    mov rax, 60\n";
+					output += "    mov rdi, "  + tokens[i+1].value +"\n";
+					output += "    syscall";
+				}
+			}
+		}
+	}
+	return output;
+}
+
 func main (){
 	args := os.Args
 	if len(args) != 2 {
 		fmt.Println("Incorrect usage. Correct usage is ...");
-		fmt.Println("Correct usage is gz <input.gz>");
+		fmt.Println("Correct usage is gz <input.genz>");
 		os.Exit(1);
 	}
 
 	dat, err := os.ReadFile(args[1]);
 	check(err);
 	tokens := tokenize(string(dat));
-	for i:=0; i< len(tokens);i++ {
-		fmt.Println(tokens[i].TokenType, tokens[i].value);
-	}
-
+	file, err := os.Create("out.asm");
+	check(err);
+	file.Write([]byte(token_to_asm(tokens)));
 	os.Exit(0);
 
 
